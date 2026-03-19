e8185ea068f8b7c4339c64f675f537b0319abcc9
Divyansh Nama <iter.divyansh.nama@gmail.com>
2026-03-19 23:45:31 +0530

    March_19

---
+++ university-management-system-restful-api-oto/src/main/java/com/capgemini/model/entity/Department.java
@@ -0,0 +1,94 @@
+package com.capgemini.model.entity;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.NotNull;
+@Entity
+public class Department implements Serializable{
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@NotNull
+	private String name;
+	@NotNull
+	private String mailId;
+	@NotNull @OneToOne(cascade=CascadeType.ALL)
+	private Student student;
+	
+	public Department() {
+		super();
+	}
+
+	public Department(Integer id, String name, String mailId, Student student) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mailId = mailId;
+		this.student = student;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMailId() {
+		return mailId;
+	}
+
+	public void setMailId(String mailId) {
+		this.mailId = mailId;
+	}
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + ", mailId=" + mailId + ", student=" + student + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, mailId, name, student);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Department other = (Department) obj;
+		return Objects.equals(id, other.id) && Objects.equals(mailId, other.mailId) && Objects.equals(name, other.name)
+				&& Objects.equals(student, other.student);
+	}
+	
+	
+
+}
---
+++ university-management-system-restful-api-oto/src/main/java/com/capgemini/model/entity/Project.java
@@ -0,0 +1,69 @@
+package com.capgemini.model.entity;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+@Entity
+public class Project {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	@NotNull
+	private String name;
+	@NotNull
+	private String description;
+	public Project() {
+		super();
+	}
+	public Project(Integer id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Override
+	public String toString() {
+		return "Project [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id, name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Project other = (Project) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id)
+				&& Objects.equals(name, other.name);
+	}
+	
+	
+
+}
